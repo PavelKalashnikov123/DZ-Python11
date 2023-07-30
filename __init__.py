462a0d0969f13e9a253fc8f8046c01ff0c382bf0
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2023-07-30 22:09:55 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from Python_next_deep.Seminar_11.my_str import *
+from Python_next_deep.Seminar_11.archive import *
+from Python_next_deep.Seminar_11.rectangle import *
+from Python_next_deep.Seminar_11.matrix import *
+
+__all__ = ["my_str", "archive", "rectangle", "matrix", "Matrix"]
